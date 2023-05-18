7ecd7deaf5c4b4d4f790fc31cfd822486974aef2
Julian Smith <juliari114@gmail.com>
2023-05-18 01:35:44 +0000

    Fixed some flickering on initial load

--- config/createEmotionCache.ts
+++ config/createEmotionCache.ts
@@ -3,14 +3,5 @@ import createCache from '@emotion/cache'
 const isBrowser = typeof document !== 'undefined'
 
 export default function createEmotionCache() {
-  let insertionPoint
-
-  if(isBrowser) {
-    const emotionInsertionPoint = document.querySelector<HTMLMetaElement>(
-      'meta[name="emotion-insertion-point"]'
-    ) as HTMLMetaElement
-    insertionPoint = emotionInsertionPoint
-  }
-
-  return createCache({ key: 'mui-style', insertionPoint })
+  return createCache({ key: 'css', prepend: true })
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -24,8 +24,6 @@ export default function MyDocument({ emotionStyleTags }: MyDocumentProps): JSX.E
         <link rel='mask-icon' href='/safari-pinned-tab.svg' color='#5bbad5' />
         <meta name='msapplication-TileColor' content='#da532c' />
         <meta name='theme-color' content='#ffffff' />
-        <meta name='emotion-insertion-point' content='' />
-        {emotionStyleTags}
       </Head>
       <body>
         <Main />
